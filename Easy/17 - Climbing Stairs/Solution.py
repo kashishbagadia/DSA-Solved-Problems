1e8612e809b961985a69afe558e0f6527f6d854f
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-07-20 22:06:17 +0530

    Update Solution.py

--- Easy/17 - Climbing Stairs/Solution.py
+++ Easy/17 - Climbing Stairs/Solution.py
@@ -1,11 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 1:
-            return 1
-        a = 1
-        b = 1
-        for i in range(2, n + 1):
-            result = a + b
-            a = b
-            b = result
-        return b
+        path = {1:1, 2:2, 3:3}
+        
+        for i in range(4, n+1):
+            path[i] = path[i-1] + path[i-2]
+        return path[n]
