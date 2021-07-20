7df0a561e0e211ab17ee20cc34167b48e46ee350
kashishbagadia <kashishbagadiamd06@gmail.com>
2021-07-20 15:26:38 +0530

    tree inorder traversal

---
+++ Easy/20 - Binary Tree Inorder Traversal/Solution.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:        
+        # create an empty stack
+        stack = deque()
+        result = []
+        curr = root
+        while stack or curr:
+            if curr:
+                stack.append(curr)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+                result.append(curr.val)
+                curr = curr.right
+        return result
