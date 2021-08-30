5960cc1ae3a97f315b7999acfe6494830629db13
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-08-30 19:05:36 +0530

    Update Solution.py

--- Medium/002 - Add Two Numbers/Solution.py
+++ Medium/002 - Add Two Numbers/Solution.py
@@ -1,22 +1,39 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
 #         self.val = val
-#         self.left = left
-#         self.right = right
+#         self.next = next
 class Solution:
-    def postorderTraversal(self, root: TreeNode) -> List[int]:
-        if root is None:
-            return []
-        stack = [root]
-        result = []
-        while stack != []:
-            if root.left is not None:
-                stack.append(root.left)
-            if root.right is not None:
-                stack.append(root.right)
-            root = stack.pop()
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = None
+        carry = 0
+        temp = None
+        
+        
+        while l1 is not None or l2 is not None:
+            # At the start of every iteration we have to add carry if there is any
+            total_sum = carry
             
-            result.append(root.val)
-        return stack
+            if l1 is not None:
+                total_sum += l1.val
+                l1 = l1.next
+            if l2 is not None:
+                total_sum += l2.val
+                l2 = l2.next
+                
+            node = ListNode(total_sum % 10)
+            carry = total_sum // 10
+            
+            # Just for the first node
+            if temp is None:
+                temp = head = node
+            # for rest of the node
+            else:
+                temp.next = node
+                temp = temp.next
+            
+            if carry > 0:
+                temp.next = ListNode(carry)
+            
+        return head
             
