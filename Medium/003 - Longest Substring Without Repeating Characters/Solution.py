4e8d4d32766b9faaf605a5a4ec34b5faca2878a8
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-09-01 17:52:24 +0530

    Update Solution.py

--- Medium/003 - Longest Substring Without Repeating Characters/Solution.py
+++ Medium/003 - Longest Substring Without Repeating Characters/Solution.py
@@ -1,39 +1,13 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        head = None
-        carry = 0
-        temp = None
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set() # for storing unique characters only
+        l = 0
+        res = 0
         
-        
-        while l1 is not None or l2 is not None:
-            # At the start of every iteration we have to add carry if there is any
-            total_sum = carry
-            
-            if l1 is not None:
-                total_sum += l1.val
-                l1 = l1.next
-            if l2 is not None:
-                total_sum += l2.val
-                l2 = l2.next
-                
-            node = ListNode(total_sum % 10)
-            carry = total_sum // 10
-            
-            # Just for the first node
-            if temp is None:
-                temp = head = node
-            # for rest of the node
-            else:
-                temp.next = node
-                temp = temp.next
-            
-            if carry > 0:
-                temp.next = ListNode(carry)
-            
-        return head
-            
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l+=1
+            charSet.add(s[r])
+            res = max(res, r-l+1)
+        return res
