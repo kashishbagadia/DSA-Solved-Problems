9ffd293fb8c509aa6d7ae29788fa34c3171d9ca9
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-09-13 16:16:22 +0530

    Update Solution.py

--- Easy/112 - Path Sum/Solution.py
+++ Easy/112 - Path Sum/Solution.py
@@ -5,21 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minDepth(self, root: TreeNode) -> int:    
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        if not root:
+            return False
+        targetSum -= root.val
         
-        def helper(node):
-            
-            if not node:
-                return 0
-            
-            l_height = helper(node.left)
-            r_height = helper(node.right)
-            
-            if (l_height == 0):
-                return r_height + 1
-            elif (r_height == 0):
-                return l_height + 1
-            else:
-                return (min(l_height, r_height) + 1)
-            
-        return helper(root)
+        if not root.left and not root.right:
+            return targetSum == 0
+        else:
+            return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
