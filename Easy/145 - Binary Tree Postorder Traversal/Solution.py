2b13e3bb23edfc25fa373aa09019e309ab6a6b3c
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-09-19 13:54:27 +0530

    Code updated

--- Easy/145 - Binary Tree Postorder Traversal/Solution.py
+++ Easy/145 - Binary Tree Postorder Traversal/Solution.py
@@ -11,12 +11,12 @@ class Solution:
         stack = [root]
         result = []
         while stack != []:
+            root = stack.pop()            
+            result.insert(0, root.val)
             if root.left is not None:
                 stack.append(root.left)
             if root.right is not None:
                 stack.append(root.right)
-            root = stack.pop()
             
-            result.append(root.val)
-        return stack
+        return result
             
