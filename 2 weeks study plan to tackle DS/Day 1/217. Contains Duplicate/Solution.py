3af9fe6616ffe9dadf3ea5255661999a8dbc8936
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-09-19 14:45:45 +0530

    Update Solution.py

--- 2 weeks study plan to tackle DS/Day 1/217. Contains Duplicate/Solution.py
+++ 2 weeks study plan to tackle DS/Day 1/217. Contains Duplicate/Solution.py
@@ -1,20 +1,7 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def preorderTraversal(self, root: TreeNode) -> List[int]:
-        if root is None:
-            return []
-        stack = [root]
-        result = []
-        while stack != []:
-            root = stack.pop()
-            result.append(root.val)
-            if root.right is not None:
-                stack.append(root.right)
-            if root.left is not None:
-                stack.append(root.left)
-        return result
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums = sorted(nums)
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
