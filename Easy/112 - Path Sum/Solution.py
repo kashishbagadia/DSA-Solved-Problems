5599b3b52587d3a272daf5c529ea0c7807f71bc1
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-09-19 14:54:36 +0530

    Update Solution.py

--- Easy/112 - Path Sum/Solution.py
+++ Easy/112 - Path Sum/Solution.py
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def minDepth(self, root: TreeNode) -> int:    
-        
-        def helper(node):
-            
-            if not node:
-                return 0
-            
-            l_height = helper(node.left)
-            r_height = helper(node.right)
-            
-            if (l_height == 0):
-                return r_height + 1
-            elif (r_height == 0):
-                return l_height + 1
-            else:
-                return (min(l_height, r_height) + 1)
-            
-        return helper(root)
-=======
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
