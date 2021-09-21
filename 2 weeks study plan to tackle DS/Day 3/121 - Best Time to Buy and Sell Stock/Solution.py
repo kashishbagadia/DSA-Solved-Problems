845078023cca831de9111f06e40735dd6a4d1212
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-09-21 14:59:13 +0530

    Update Solution.py

--- 2 weeks study plan to tackle DS/Day 3/121 - Best Time to Buy and Sell Stock/Solution.py
+++ 2 weeks study plan to tackle DS/Day 3/121 - Best Time to Buy and Sell Stock/Solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0
-        minsoFar = prices[0]
-        for i in range(0, len(prices)):
-            minsoFar = min(minsoFar, prices[i])
-            profit = prices[i] - minsoFar
-            maxProfit = max(maxProfit, profit)
-        return maxProfit
+        min_so_far = prices[0]
+        max_profit = 0
+        for i in range(len(prices)):
+            min_so_far = min(min_so_far, prices[i])
+            profit = prices[i] - min_so_far
+            max_profit = max(max_profit, profit)
+        return max_profit
