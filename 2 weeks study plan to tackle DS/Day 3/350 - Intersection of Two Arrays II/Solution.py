823591859848f15daefb0c34e873113b198d778f
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-09-21 20:57:02 +0530

    Update Solution.py

--- 2 weeks study plan to tackle DS/Day 3/350 - Intersection of Two Arrays II/Solution.py
+++ 2 weeks study plan to tackle DS/Day 3/350 - Intersection of Two Arrays II/Solution.py
@@ -1,9 +1,15 @@
+from collections import Counter
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0
-        minsoFar = prices[0]
-        for i in range(0, len(prices)):
-            minsoFar = min(minsoFar, prices[i])
-            profit = prices[i] - minsoFar
-            maxProfit = max(maxProfit, profit)
-        return maxProfit
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+    
+        ans = []
+        d = Counter(nums2)
+
+        for x in nums1:
+            if d[x]>0:
+                ans.append(x)
+                d[x] -= 1
+        
+        return ans
