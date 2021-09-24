ffc4ce124607e8e149e5612ec50eb873b1dcc534
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-09-24 09:04:29 +0530

    Update Solution.py

--- 2 weeks study plan to tackle DS/Day 6/387 - First Unique Character in a String/Solution.py
+++ 2 weeks study plan to tackle DS/Day 6/387 - First Unique Character in a String/Solution.py
@@ -1,22 +1,10 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        ans = []
-        first_line = [1]
-        second_line = [1, 1]
-        if numRows == 0:
-            return ans
-        ans.append(first_line)
-        if numRows == 1:
-            return ans
-        ans.append(second_line)
-        if numRows == 2:
-            return ans
-        for i in range(2, numRows):
-            current_line = list(range(i + 1))
-            current_line[0] = 1
-            current_line[i] = 1
-            for j in range(1, i):
-                current_line[j] = ans[i-1][j-1] + ans[i-1][j]
-            ans.append(current_line)
-        return ans
+    def firstUniqChar(self, s: str) -> int:
+        for i in range(len(s)):
+            check = s[0:i] + s[i+1:]
+            if s[i] in check:
+                continue
+            else:
+                return i
+        return -1
             
