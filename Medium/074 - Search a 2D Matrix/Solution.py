fb4828579f42e4023ebb03720e34582774260f0e
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-10-07 22:27:24 +0530

    Update Solution.py

--- Medium/074 - Search a 2D Matrix/Solution.py
+++ Medium/074 - Search a 2D Matrix/Solution.py
@@ -1,13 +1,6 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        charSet = set() # for storing unique characters only
-        l = 0
-        res = 0
-        
-        for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
-                l+=1
-            charSet.add(s[r])
-            res = max(res, r-l+1)
-        return res
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in range(len(matrix)):
+            if target in matrix[i]:
+                return True
+        return False
