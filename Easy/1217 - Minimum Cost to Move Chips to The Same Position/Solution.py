64485724d3b2fecd64a1c6cb956b325ccd7274fd
kashishbagadia <kashishbagadiamd06@gmail.com>
2021-12-06 16:16:06 +0530

    python file

--- Easy/1217 - Minimum Cost to Move Chips to The Same Position/Solution.py
+++ Easy/1217 - Minimum Cost to Move Chips to The Same Position/Solution.py
@@ -1,22 +1,13 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def postorderTraversal(self, root: TreeNode) -> List[int]:
-        if root is None:
-            return []
-        stack = [root]
-        result = []
-        while stack != []:
-            root = stack.pop()            
-            result.insert(0, root.val)
-            if root.left is not None:
-                stack.append(root.left)
-            if root.right is not None:
-                stack.append(root.right)
-            
-        return result
-            
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        even = 0
+        odd = 0
+        for i in position:
+            if i % 2 == 0:
+                even +=1
+            else:
+                odd += 1
+        if even == len(position) or odd == len(position):
+            return 0
+        else:
+            return min(even, odd)
