134f89df9cb11c5a3d2221ab414fb90338462dad
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2022-05-18 14:00:40 +0530

    Added solution - LeetHub

---
+++ Find minimum and maximum element in an array - GFG/find-minimum-and-maximum-element-in-an-array.java
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class pair  
+{  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            pair product = obj.getMinMax(a, n); 
+            System.out.println(product.first+" "+product.second);
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+ class pair  
+{  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+} */
+
+class Compute 
+{
+    static pair getMinMax(long a[], long n)  
+    {
+        long min = a[0], max = a[0];
+       int i;
+       if(n == 1){
+           max = a[0];
+           min = a[1];
+           return new pair(min, max);
+       }
+       if(a[0] > a[1]){
+           max = a[0];
+           min = a[1];
+       }else{
+           max = a[1];
+           min = a[0];
+       }
+       for(i = 2; i < n; i++){
+           if(a[i] > max) max = a[i];
+           else if(a[i] < min) min = a[i];
+       }
+       return new pair(min, max);
+    }
+}
