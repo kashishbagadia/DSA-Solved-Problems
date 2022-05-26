e18b87fc0e0ab068d813df7e1e222347747e0704
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2022-05-26 08:01:16 +0530

    Time: 3 ms (52.74%), Space: 82.9 MB (54.42%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0], max_P = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(buy > prices[i]) buy = prices[i];
+            else if(prices[i] - buy > max_P) max_P = prices[i] - buy;
+        }
+        return max_P;
+        
+    }
+}
