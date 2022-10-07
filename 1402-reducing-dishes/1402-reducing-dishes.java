3583e8610632bad4d57d804798e674b0595eccfb
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2022-10-07 09:30:43 +0530

    Time: 8 ms (46.87%), Space: 42.3 MB (58.15%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,11 +1,24 @@
 class Solution {
     public int maxSatisfaction(int[] satisfaction) {
         Arrays.sort(satisfaction);
-        int n = satisfaction.length;
-        int dp[][] = new int[n][n];
-        for(int rows[] : dp) Arrays.fill(rows, -1);
-        return solve(satisfaction, 0, 0, dp);        
+
+        return solveTab(satisfaction);        
     }
+    public int solveTab(int[] satisfaction){
+        int n = satisfaction.length;
+        int dp[][] = new int[n+1][n+1];
+        for(int rows[] : dp) Arrays.fill(rows, 0);
+
+        for(int index = n-1; index >= 0; index--){
+            for(int time = index; time>=0; time--){
+                int include = satisfaction[index]*(time+1) + dp[index+1][time+1];
+                int exclude = dp[index+1][time];
+                dp[index][time] = Math.max(include, exclude); 
+            }            
+        }
+        return dp[0][0];
+    } 
+    
     public int solve(int[] satisfaction, int index, int time, int[][] dp){
         if(index == satisfaction.length) return 0;
         if(dp[index][time]!=-1) return dp[index][time];
@@ -13,4 +26,5 @@ class Solution {
         int exclude = solve(satisfaction, index+1, time, dp);
         return dp[index][time] = Math.max(include, exclude);
     }
+    
 }
