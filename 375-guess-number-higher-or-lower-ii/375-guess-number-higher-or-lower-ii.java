148c51e2ef1acc6890dd616ba6e8652605fe7b71
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2022-10-10 11:15:41 +0530

    Time: 38 ms (74.38%), Space: 42.4 MB (26.69%) - LeetHub

--- 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.java
+++ 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.java
@@ -1,8 +1,32 @@
 class Solution {
     public int getMoneyAmount(int n) {
-        int[][] dp = new int[n+1][n+1];
-        for(int rows[] : dp) Arrays.fill(rows, -1);
-        return solve(1, n, dp);        
+        // int[][] dp = new int[n+1][n+1];
+        // for(int rows[] : dp) Arrays.fill(rows, -1);
+        // return solve(1, n, dp);   
+        return solveTab(n);
+           
+    }
+    
+    public int solveTab(int n){
+        int[][] dp = new int[n+2][n+2];
+        for(int rows[] : dp) Arrays.fill(rows, 0);
+        for(int start = n; start>=1; start--){
+            for(int end = start; end <=n; end++){
+                if(start == end) continue;
+                else{
+                    int max = Integer.MAX_VALUE;
+        
+                    for(int i = start; i<=end; i++){
+                        max = Math.min(max, i + Math.max(dp[start][i-1], dp[i+1][end]));
+                    }
+
+                    dp[start][end] = max;
+                    
+                }
+            }
+        }
+        return dp[1][n];
+        
     }
     
     public int solve(int start, int end, int[][]dp){
